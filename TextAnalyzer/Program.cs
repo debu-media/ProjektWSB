40f8ef72ab207d9ab3e88a81269b03b75af69097
malesiak <jakubmalesa@gmail.com>
2019-10-05 16:25:15 +0200

    fixes #2 - Download file from internet

--- TextAnalyzer/Program.cs
+++ TextAnalyzer/Program.cs
@@ -21,7 +21,7 @@ namespace TextAnalyzer
                 switch (Menu_option)
                 {
                     case 1:
-                       //string Text = GetFileFromInternet();
+                       string Text = GetFileFromInternet();
                         GetMenu();
                         Menu_option = Convert.ToInt32(Console.ReadLine());
                         break;
@@ -46,7 +46,14 @@ namespace TextAnalyzer
             Console.WriteLine("7 - Save statistics to the file");
             Console.WriteLine("8 - Exit");
         }
-        
+        public static String GetFileFromInternet()
+        {
+            WebClient webClient = new WebClient();
+            webClient.DownloadFile("https://s3.zylowski.net/public/input/1.txt", @"stringfile.txt");
+            string text = System.IO.File.ReadAllText(@"stringfile.txt");
+            File.Delete("stringfile.txt");
+            return text;
+        }
 
     }
 }
