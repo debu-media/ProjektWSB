6ce23a76d69b791967d75e8062ca75f44525f95f
debu-media <patryk@debu.pl>
2019-10-05 15:02:57 +0200

    Fixes #1 - Add a menu program

--- TextAnalyzer/Program.cs
+++ TextAnalyzer/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +10,43 @@ namespace TextAnalyzer
 {
     class Program
     {
+
         static void Main(string[] args)
         {
+            int Program_run = 1;
+            GetMenu();
+           int  Menu_option = Convert.ToInt32(Console.ReadLine());
+            while (Program_run == 1)
+            {
+                switch (Menu_option)
+                {
+                    case 1:
+                       //string Text = GetFileFromInternet();
+                        GetMenu();
+                        Menu_option = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    default:
+                        Console.WriteLine("Musisz wybrać opcję \n");
+                        GetMenu();
+                        Menu_option = Convert.ToInt32(Console.ReadLine());
+                        break;
+                }
+
+            }
         }
+        static void GetMenu()
+        {
+            Console.WriteLine("   Menu \n==========\n");
+            Console.WriteLine("1 - Get file from internet");
+            Console.WriteLine("2 - Count letters");
+            Console.WriteLine("3 - Count words");
+            Console.WriteLine("4 - Count punctuation marks");
+            Console.WriteLine("5 - Count sentences");
+            Console.WriteLine("6 -  Generate report about count of every letter in the file");
+            Console.WriteLine("7 - Save statistics to the file");
+            Console.WriteLine("8 - Exit");
+        }
+        
+
     }
 }
