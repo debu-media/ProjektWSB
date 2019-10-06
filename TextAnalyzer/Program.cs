29aec5338ccae0edfe2094a5e3812b6dbac1eb13
debu-media <patryk@debu.pl>
2019-10-06 20:26:03 +0200

    fixes #7 - Generate report about count of every letter in the file

--- TextAnalyzer/Program.cs
+++ TextAnalyzer/Program.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace TextAnalyzer
@@ -79,6 +80,19 @@ namespace TextAnalyzer
                         Menu_option = Convert.ToInt32(Console.ReadLine());
 
                         break;
+                    case 6:
+                        if (Text != "Null")
+                        {
+                            GenerateRaport(Text);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Sorry, the file does not exist. \n \n Choose option no. 1 and try again \n\n");
+                        }
+                        GetMenu();
+                        Menu_option = Convert.ToInt32(Console.ReadLine());
+                        
+                        break;
                     default:
                         Console.WriteLine("You must select an option from the menu \n");
                         GetMenu();
@@ -146,6 +160,167 @@ namespace TextAnalyzer
             var translateArraySourceTexts = text.Split(new string[] { ".", "!?", "!", "?", "...", "?!", ".!" }, StringSplitOptions.RemoveEmptyEntries);
             var count = translateArraySourceTexts.Length;
             return count;
+        }
+            public static void  GenerateRaport(string text)
+        {
+            string pattern = @"[Aa]";
+            int A=0, B=0, C=0, D=0, E=0, F=0,G=0, H=0, I=0, J=0, K=0, L=0, M=0, N=0, O=0, P=0, Q=0, R=0, S=0, T=0, U=0, V=0, W=0, X=0, Y=0, Z=0;
+            Regex rg = new Regex(pattern);
+            MatchCollection matchedA = rg.Matches(text);
+            for (int count = 0; count < matchedA.Count; count++)
+                A++;
+            pattern = @"[Bb]";
+            rg = new Regex(pattern);
+            MatchCollection matchedB = rg.Matches(text);
+            for (int count = 0; count < matchedB.Count; count++)
+                B++;
+            pattern = @"[Cc]";
+            rg = new Regex(pattern);
+            MatchCollection matchedC = rg.Matches(text);
+            for (int count = 0; count < matchedC.Count; count++)
+                C++;
+            pattern = @"[Dd]";
+            rg = new Regex(pattern);
+            MatchCollection matchedD = rg.Matches(text);
+            for (int count = 0; count < matchedD.Count; count++)
+                D++;
+            pattern = @"[Ee]";
+            rg = new Regex(pattern);
+            MatchCollection matchedE = rg.Matches(text);
+            for (int count = 0; count < matchedE.Count; count++)
+                E++;
+            pattern = @"[Ff]";
+            rg = new Regex(pattern);
+            MatchCollection matchedF = rg.Matches(text);
+            for (int count = 0; count < matchedF.Count; count++)
+                F++;
+            pattern = @"[Gg]";
+            rg = new Regex(pattern);
+            MatchCollection matchedG = rg.Matches(text);
+            for (int count = 0; count < matchedG.Count; count++)
+                G++;
+            pattern = @"[Hh]";
+            rg = new Regex(pattern);
+            MatchCollection matchedH = rg.Matches(text);
+            for (int count = 0; count < matchedH.Count; count++)
+                H++;
+            pattern = @"[Ii]";
+            rg = new Regex(pattern);
+            MatchCollection matchedI = rg.Matches(text);
+            for (int count = 0; count < matchedI.Count; count++)
+                I++;
+            pattern = @"[Jj]";
+            rg = new Regex(pattern);
+            MatchCollection matchedJ = rg.Matches(text);
+            for (int count = 0; count < matchedJ.Count; count++)
+                J++;
+            pattern = @"[Kk]";
+            rg = new Regex(pattern);
+            MatchCollection matchedK = rg.Matches(text);
+            for (int count = 0; count < matchedK.Count; count++)
+                K++;
+            pattern = @"[Ll]";
+            rg = new Regex(pattern);
+            MatchCollection matchedL = rg.Matches(text);
+            for (int count = 0; count < matchedL.Count; count++)
+                L++;
+            pattern = @"[Mm]";
+            rg = new Regex(pattern);
+            MatchCollection matchedM = rg.Matches(text);
+            for (int count = 0; count < matchedM.Count; count++)
+                M++;
+            pattern = @"[Nn]";
+            rg = new Regex(pattern);
+            MatchCollection matchedN = rg.Matches(text);
+            for (int count = 0; count < matchedN.Count; count++)
+                N++;
+            pattern = @"[Oo]";
+            rg = new Regex(pattern);
+            MatchCollection matchedO = rg.Matches(text);
+            for (int count = 0; count < matchedO.Count; count++)
+                O++;
+            pattern = @"[Pp]";
+            rg = new Regex(pattern);
+            MatchCollection matchedP = rg.Matches(text);
+            for (int count = 0; count < matchedP.Count; count++)
+                P++;
+            pattern = @"[Bb]";
+            rg = new Regex(pattern);
+            MatchCollection matchedQ = rg.Matches(text);
+            for (int count = 0; count < matchedQ.Count; count++)
+                Q++;
+            pattern = @"[Rr]";
+            rg = new Regex(pattern);
+            MatchCollection matchedR = rg.Matches(text);
+            for (int count = 0; count < matchedR.Count; count++)
+                R++;
+            pattern = @"[Ss]";
+            rg = new Regex(pattern);
+            MatchCollection matchedS = rg.Matches(text);
+            for (int count = 0; count < matchedS.Count; count++)
+                S++;
+            pattern = @"[Tt]";
+            rg = new Regex(pattern);
+            MatchCollection matchedT = rg.Matches(text);
+            for (int count = 0; count < matchedT.Count; count++)
+                T++;
+            pattern = @"[Uu]";
+            rg = new Regex(pattern);
+            MatchCollection matchedU = rg.Matches(text);
+            for (int count = 0; count < matchedU.Count; count++)
+                U++;
+            pattern = @"[Vv]";
+            rg = new Regex(pattern);
+            MatchCollection matchedV = rg.Matches(text);
+            for (int count = 0; count < matchedV.Count; count++)
+                V++;
+            pattern = @"[Ww]";
+            rg = new Regex(pattern);
+            MatchCollection matchedW = rg.Matches(text);
+            for (int count = 0; count < matchedW.Count; count++)
+                W++;
+            pattern = @"[Xx]";
+            rg = new Regex(pattern);
+            MatchCollection matchedX = rg.Matches(text);
+            for (int count = 0; count < matchedX.Count; count++)
+                X++;
+            pattern = @"[Yy]";
+            rg = new Regex(pattern);
+            MatchCollection matchedY = rg.Matches(text);
+            for (int count = 0; count < matchedY.Count; count++)
+                Y++;
+            pattern = @"[Zz]";
+            rg = new Regex(pattern);
+            MatchCollection matchedZ = rg.Matches(text);
+            for (int count = 0; count < matchedZ.Count; count++)
+                Z++;
+            Console.WriteLine("A: " + A);
+            Console.WriteLine("B: " + B);
+            Console.WriteLine("C: " + C);
+            Console.WriteLine("D: " + D);
+            Console.WriteLine("E: " + E);
+            Console.WriteLine("F: " + F);
+            Console.WriteLine("G: " + G);
+            Console.WriteLine("H: " + H);
+            Console.WriteLine("I: " + I);
+            Console.WriteLine("J: " + J);
+            Console.WriteLine("K: " + K);
+            Console.WriteLine("L: " + L);
+            Console.WriteLine("M: " + M);
+            Console.WriteLine("N: " + N);
+            Console.WriteLine("O: " + O);
+            Console.WriteLine("P: " + P);
+            Console.WriteLine("Q: " + Q);
+            Console.WriteLine("R: " + R);
+            Console.WriteLine("S: " + S);
+            Console.WriteLine("T: " + T);
+            Console.WriteLine("U: " + U);
+            Console.WriteLine("V: " + V);
+            Console.WriteLine("W: " + W);
+            Console.WriteLine("X: " + X);
+            Console.WriteLine("Y: " + Y);
+            Console.WriteLine("Z: " + Z);
+
         }
     }
 }
